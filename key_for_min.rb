898c1e14490d2fb4ab82c9bafe9f396545e5704c
Ajak Cyer <ajakcyer97@gmail.com>
2020-09-06 04:50:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,6 @@
 def key_for_min_value(name_hash)
   
   name_hash.each do |key, numeric|
-    min_value = -1
-    if min_value <= numeric
-      min_value = numeric
-    end
+    if min_value
   end
 end
