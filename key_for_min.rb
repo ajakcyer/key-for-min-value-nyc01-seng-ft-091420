6a1b19726908b39ddadc98b47ceeb507cc29ccae
Ajak Cyer <ajakcyer97@gmail.com>
2020-09-06 04:29:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_value = 1
+  
   name_hash.each do |key, numeric|
-    if min_value < numeric
-      min_value = numeric
+    if 
+      
     end
   end
 end
