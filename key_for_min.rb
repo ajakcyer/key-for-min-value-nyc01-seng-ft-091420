8ec02e480fcb3a78dd96609e78616b5498bd8d5c
Ajak Cyer <ajakcyer97@gmail.com>
2020-09-06 05:11:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -14,5 +14,6 @@ def key_for_min_value(name_hash)
   end
   min_key
 end
-  # prereqs: iterators, hashes, conditional logic
+
+# prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
